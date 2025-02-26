6584641b2e6c179f40513cc76dc1b56f0c5df244
Alpha, Orderly <55572245+alphaorderly@users.noreply.github.com>
2025-02-26 17:22:11 +0900

    Time: 3674 ms (5.12%) | Memory: 28.6 MB (76.14%) - LeetSync

---
+++ 2000-minimum-speed-to-arrive-on-time/minimum-speed-to-arrive-on-time.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minSpeedOnTime(self, dist: List[int], hour: float) -> int:
+        def calc_hour(speed: int) -> int:
+            ret = 0
+
+            for i, d in enumerate(dist):
+                if i < len(dist) - 1:
+                    ret += math.ceil(d / speed)
+                else:
+                    ret += d / speed
+
+            return ret
+
+        left = 1
+        right = 10 ** 12
+        
+        ans = -1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            takes = calc_hour(mid)
+
+            if takes > hour:
+                left = mid + 1
+            else:
+                ans = mid
+                right = mid - 1
+
+        return ans
