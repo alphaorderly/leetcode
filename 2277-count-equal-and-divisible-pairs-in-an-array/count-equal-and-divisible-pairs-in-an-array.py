11d947492899b3f6caf02606c665908e39514833
Alpha, Orderly <55572245+alphaorderly@users.noreply.github.com>
2025-04-17 10:55:58 +0900

    Time: 0 ms (100.00%) | Memory: 17.9 MB (32.33%) - LeetSync

--- 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.py
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.py
@@ -1,12 +1,23 @@
+import math
+from collections import defaultdict
+
 class Solution:
     def countPairs(self, nums: List[int], k: int) -> int:
         N = len(nums)
-
         ans = 0
 
-        for i in range(N - 1):
-            for j in range(i + 1, N):
-                if nums[i] == nums[j] and (i * j) % k == 0:
-                    ans += 1
+        gcd_counts = defaultdict(lambda: defaultdict(int)) 
 
+        for j in range(N):
+            val = nums[j]
+            
+            g_j = math.gcd(j, k)
+            
+            for stored_g_i, count in gcd_counts[val].items():
+                if (stored_g_i * g_j) % k == 0:
+                    ans += count
+            
+            gcd_counts[val][g_j] += 1
+            
         return ans
+
