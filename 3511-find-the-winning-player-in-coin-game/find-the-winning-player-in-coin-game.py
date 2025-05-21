28e9cc68511741adfc2a8b8db316227427172e4f
Alpha, Orderly <55572245+alphaorderly@users.noreply.github.com>
2025-05-21 17:30:49 +0900

    Time: 0 ms (100.00%) | Memory: 18 MB (20.96%) - LeetSync

--- 3511-find-the-winning-player-in-coin-game/find-the-winning-player-in-coin-game.py
+++ 3511-find-the-winning-player-in-coin-game/find-the-winning-player-in-coin-game.py
@@ -1,11 +1,8 @@
 class Solution:
     def winningPlayer(self, x: int, y: int) -> str:
-        turn = 'Bob'
-        
-        while x >= 1 and y >= 4:
-            x -= 1
-            y -= 4
+        s = x
+        t = y // 4
 
-            turn = 'Bob' if turn == 'Alice' else 'Alice'
+        m = min(s, t)
 
-        return turn
+        return 'Alice' if m % 2 else 'Bob'
