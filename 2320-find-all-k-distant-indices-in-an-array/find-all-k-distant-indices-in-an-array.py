0bac16e122676c00d09bd19f57de4d990d0044b7
Alpha, Orderly <55572245+alphaorderly@users.noreply.github.com>
2025-06-24 16:21:13 +0900

    Time: 115 ms (44.08%) | Memory: 18.1 MB (46.56%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.py
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.py
@@ -3,12 +3,11 @@ class Solution:
         self, nums: List[int], key: int, k: int
     ) -> List[int]:
         res = []
-        r = 0  # unjudged minimum index
         n = len(nums)
-        for j in range(n):
-            if nums[j] == key:
-                l = max(r, j - k)
-                r = min(n - 1, j + k) + 1
-                for i in range(l, r):
+        # traverse number pairs
+        for i in range(n):
+            for j in range(n):
+                if nums[j] == key and abs(i - j) <= k:
                     res.append(i)
+                    break  # early termination to prevent duplicate addition
         return res
