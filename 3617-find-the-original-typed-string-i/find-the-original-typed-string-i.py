e1009e96daa518bd246231bd8b411b6b6f9d2b40
Alpha, Orderly <55572245+alphaorderly@users.noreply.github.com>
2025-07-01 10:24:18 +0900

    Time: 34 ms (84.23%) | Memory: 17.9 MB (6.48%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.py
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.py
@@ -1,26 +1,28 @@
 class Solution:
     def possibleStringCount(self, word: str) -> int:
-        x = []
-
         prev = None
         cnt = 0
+
+        check = []
 
         for ch in word:
             if prev is None:
                 prev = ch
                 cnt = 1
-            elif prev != ch:
-                x.append([prev, cnt])
+            elif prev == ch:
+                cnt += 1
+            else:
+                check.append((prev, cnt))
                 prev = ch
                 cnt = 1
-            else:
-                cnt += 1
 
-        x.append([prev, cnt])
+        check.append((prev, cnt))
 
-        ans = 0
+        ans = 1
+        p = 0
 
-        for _, c in x:
-            ans += c - 1
+        for _, v in check:
+            if v > 1:
+                ans += v - 1
 
-        return ans + 1
+        return ans
