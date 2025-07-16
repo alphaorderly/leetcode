3f6290ef2ef93afcc0876376f004f2b0121008f9
Alpha, Orderly <55572245+alphaorderly@users.noreply.github.com>
2025-07-16 15:54:29 +0900

    Time: 88 ms (42.64%) | Memory: 39.2 MB (54.26%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.py
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.py
@@ -3,19 +3,15 @@ class Solution:
         even_count = 0
         odd_count = 0
 
-        for i, num in enumerate(nums):
-            if num % 2:
-                odd_count += 1
-            else:
-                even_count += 1
-
         last_even = 0
         last_odd = 0
 
-        for num in nums:
+        for i, num in enumerate(nums):
             if num % 2:
+                odd_count += 1
                 last_odd = max(last_odd, last_even + 1)
             else:
+                even_count += 1
                 last_even = max(last_even, last_odd + 1)
 
         return max(even_count, odd_count, last_even, last_odd)
