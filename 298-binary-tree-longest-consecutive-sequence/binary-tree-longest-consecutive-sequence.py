0b98f3203015c4a07c0f0dd09cc7135e21b7908c
Alpha, Orderly <55572245+alphaorderly@users.noreply.github.com>
2025-07-21 15:40:02 +0900

    Time: 34 ms (5.13%) | Memory: 23.9 MB (6.41%) - LeetSync

--- 298-binary-tree-longest-consecutive-sequence/binary-tree-longest-consecutive-sequence.py
+++ 298-binary-tree-longest-consecutive-sequence/binary-tree-longest-consecutive-sequence.py
@@ -1,36 +1,26 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def longestConsecutive(self, root: Optional[TreeNode]) -> int:
         checked = set()
 
-        ans = 1
-
         def check(node: Optional[TreeNode]):
-            nonlocal ans
-
             if node in checked:
-                check(node.left)
-                check(node.right)
-                return
+                return max(check(node.left), check(node.right))
 
             if not node:
-                return
+                return 0
 
             s = [(node, 1)]
 
             checked.add(node)
+
+            max_value = 1
 
             while s:
                 p, d = s.pop()
 
                 checked.add(p)
 
-                ans = max(ans, d)
+                max_value = max(max_value, d)
 
                 if p.left and p.left.val == p.val + 1:
                     s.append((p.left, d + 1))
@@ -38,11 +28,6 @@ class Solution:
                 if p.right and p.right.val == p.val + 1:
                     s.append((p.right, d + 1))
 
-            check(node.left)
-            check(node.right)
+            return max(max_value, check(node.left), check(node.right))
 
-            return
-
-        check(root)
-
-        return ans
+        return check(root)
