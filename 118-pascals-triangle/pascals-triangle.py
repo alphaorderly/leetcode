5fbdf11e95a3513209e795c5c67e4b87cdb32c98
Alpha, Orderly <55572245+alphaorderly@users.noreply.github.com>
2025-08-01 11:59:49 +0900

    Time: 0 ms (100.00%) | Memory: 17.9 MB (16.18%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,5 +1,22 @@
-from math import factorial as f
-
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        return [[f(i) // (f(j) * f(i-j)) for j in range(i+1)] for i in range(numRows)]
+        if numRows == 1:
+            return [[1]]
+
+        if numRows == 2:
+            return [[1], [1, 1]]
+
+        ans = [
+            [1], [1, 1]
+        ]
+
+        for row in range(2, numRows):
+            app = []
+            for col in range(row + 1):
+                if col == 0 or col == row:
+                    app.append(1)
+                else:
+                    app.append(ans[row - 1][col - 1] + ans[row - 1][col])
+            ans.append(app)
+
+        return ans
