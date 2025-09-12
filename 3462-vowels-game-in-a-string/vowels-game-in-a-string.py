2982627ae471d0831078a7b02959d250933c5218
Alpha, Orderly <55572245+alphaorderly@users.noreply.github.com>
2025-09-12 11:41:39 +0900

    Time: 0 ms (100.00%) | Memory: 18.1 MB (50.30%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
@@ -1,8 +1,7 @@
+vowels = {'a', 'e', 'i', 'o', 'u'}
+
 class Solution:
     def doesAliceWin(self, s: str) -> bool:
-
-        vowels = {'a', 'e', 'i', 'o', 'u'}
-
         for v in vowels:
             if v in s:
                 return True
