86675811f6bcaec7a653c23138dbc3f0efe6222f
Alpha, Orderly <55572245+alphaorderly@users.noreply.github.com>
2025-09-16 13:40:08 +0900

    Time: 351 ms (11.95%) | Memory: 31.8 MB (22.83%) - LeetSync

---
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
+        s = []
+
+        for num in nums:
+            if not s:
+                s.append(num)
+                continue
+
+            while s:
+                g = gcd(s[-1], num)
+                if g > 1:
+                    v = s.pop()
+                    num = v * num // g
+                else:
+                    break
+
+            s.append(num)
+
+            print(g)
+
+        return s
