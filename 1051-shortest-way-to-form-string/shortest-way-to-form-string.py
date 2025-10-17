10fbf010a8194b153e659365d21b728177bd6bcb
Alpha, Orderly <55572245+alphaorderly@users.noreply.github.com>
2025-10-17 20:27:03 +0900

    Time: 0 ms (100.00%) | Memory: 18 MB (29.80%) - LeetSync

--- 1051-shortest-way-to-form-string/shortest-way-to-form-string.py
+++ 1051-shortest-way-to-form-string/shortest-way-to-form-string.py
@@ -1,24 +1,27 @@
+from collections import defaultdict
+from bisect import bisect_left
+
 class Solution:
     def shortestWay(self, source: str, target: str) -> int:
-        placement = defaultdict(list)
+        if not target:
+            return 0
 
+        placement = defaultdict(list)
         for i, s in enumerate(source):
             placement[s].append(i)
 
-        ans = 1
-        position = 0
-
+        ans, position = 1, 0
         for ch in target:
             if ch not in placement:
                 return -1
 
-            current_index = bisect_left(placement[ch], position)
-
-            if current_index >= len(placement[ch]):
-                ans += 1
+            idxs = placement[ch]
+            j = bisect_left(idxs, position)
+            if j == len(idxs):
+                ans += 1          # 새 라운드 시작
                 position = 0
-                current_index = bisect_left(placement[ch], position)
+                j = bisect_left(idxs, position)
 
-            position = placement[ch][current_index] + 1
+            position = idxs[j] + 1
 
         return ans
