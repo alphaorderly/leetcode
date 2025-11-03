75da38ecc958d9d56d6cdc6528487288bcfdc411
Alpha, Orderly <55572245+alphaorderly@users.noreply.github.com>
2025-11-03 09:51:54 +0900

    Time: 47 ms (95.56%) | Memory: 26.4 MB (62.73%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
@@ -1,16 +1,18 @@
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
-        stack: List[int] = []
+        curr = None
+
         ans = 0
-        
-        for i, c in enumerate(colors):
-            if not stack or stack[-1][1] != c:
-                stack.append((neededTime[i], c))
+
+        for i, v in enumerate(colors):
+            if curr is None or colors[curr] != v:
+                curr = i
             else:
-                if stack[-1][0] <= neededTime[i]:
-                    ans += stack.pop()[0]
-                    stack.append((neededTime[i], c))
+                if neededTime[curr] < neededTime[i]:
+                    ans += neededTime[curr]
+                    curr = i
                 else:
                     ans += neededTime[i]
 
         return ans
+
